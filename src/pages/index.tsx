368c6a483976e7d372d5f5ac5e444018304dd585
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-04-20 21:43:38 -0300

    feat:ssA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
+import { useEffect } from "react";
+
+export default function Home(props) {
+  //SPA -> Sempre que algo mudar algo acontece, efeito colateral, side effect
+  console.log(props.episodes);
+
   return <h1>Index</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
